2001be3998142ba4f3c2ee6c0ad4f5071aa0d930
Vadim Dvorovenko <Vadimon@mail.ru>
2016-04-27 10:53:38 +0700

    Initial commit

---
+++ lib.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Tool for creating demo version with specific questions of quiz with random questions.
+ *
+ * @package    local_quizdemo
+ * @copyright  2016 Vadim Dvorovenko <Vadimon@mail.ru>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+/**
+ * Adds quizdemo item into quiz module.
+ *
+ * @param navigation_node $nav navigation node object
+ * @param context $context course context object
+ */
+function local_quizdemo_extend_settings_navigation(navigation_node $nav, context $context) {
+
+}
